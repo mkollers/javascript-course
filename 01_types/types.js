cca95f91beaebd6d358dd48146b0f73d2e1ccbe8
Markus Kollers <m.kollers@vertical.de>
2019-08-19 19:55:12 +0200

    Add some questions for types

---
+++ 01_types/types.js
@@ -0,0 +1,97 @@
+/* 
+Frage 1
+Wo ist der Unterschied zwischen const, let und var?
+*/
+// const x = 'Foo';
+// let y = 'Bar';
+// var z = 'Schnitzel';
+
+/*
+Frage 2
+Was ist das Ergebnis?
+*/ 
+// const x = 'Schnitzel';
+// console.log(typeof x);
+
+/*
+Frage 3
+Wo ist der Unterschied zwischen einfachen und doppelten Anführungszeichen?
+*/
+// const x = 'Foo';
+// const y = "Bar";
+// console.log(typeof x);
+// console.log(typeof y);
+
+/* 
+Frage 4
+Was ist das Ergebnis?
+*/
+// const x = 42;
+// console.log(typeof x);
+
+/* 
+Frage 5
+Was ist das Ergebnis?
+*/
+// const x = 42.42;
+// console.log(typeof x);
+
+/*
+Frage 6
+Was ist das Ergebnis?
+*/
+// var x;
+// console.log(typeof x);
+
+/*
+Frage 7
+Was ist das Ergebnis?
+*/
+// const x = null;
+// console.log(typeof x);
+
+/*
+Frage 8
+Was ist das Ergebnis?
+*/
+// const x = true;
+// console.log(typeof x);
+
+/* 
+Frage 9
+Was ist das Ergebnis?
+*/
+// let x = true;
+// x = 'Schnitzel';
+// console.log(typeof x); 
+
+/*
+Frage 10
+Was ist das Ergebnis?
+*/
+// const drink = {
+//     name: 'beer',
+//     alcohol: 11
+// };
+// console.log(typeof drink);
+
+/*
+Frage 11
+Was ist das Ergebnis?
+*/
+// const x = ['Foo', 'Bar', 42];
+// console.log(typeof x);
+
+/*
+Frage 12
+Was ist das Ergebnis?
+*/
+// const x = ['Foo', 'Bar', 42];
+// console.log(x);
+
+/*
+Frage 13
+Was ist das Ergebnis?
+*/
+// const x = new Date('2019-01-01');
+// console.log(typeof x);

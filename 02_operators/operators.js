f76a1e1e2a90ce725e524efc646b95fcd4d1f85c
Markus Kollers <m.kollers@vertical.de>
2019-09-03 13:52:17 +0200

    Add operator questions

---
+++ 02_operators/operators.js
@@ -0,0 +1,64 @@
+/*
+Frage 1
+Was ist das Ergebnis?
+*/
+// const x = '42' == "42";
+// console.log(x);
+
+/*
+Frage 2
+Was ist das Ergebnis?
+*/
+// const x = '42' == 42;
+// console.log(x);
+
+/*
+Frage 3
+Was ist das Ergebnis?
+*/
+// const x = '42' === 42;
+// console.log(x);
+
+/*
+Frage 4
+Was ist das Ergebnis?
+*/
+// const x = { a: '42' } === { a: '42' };
+// console.log(x);
+
+/*
+Frage 5
+Was ist das Ergebnis?
+*/
+// const x = ['42'] === ['42'];
+// console.log(x);
+
+/*
+Frage 6
+Was ist das Ergebnis?
+*/
+// const x = 4 + '2';
+// console.log(x);
+
+/*
+Frage 7
+Was ist das Ergebnis?
+*/
+// const x = '4' - 2;
+// console.log(x);
+
+/*
+Frage 8
+Was ist das Ergebnis?
+*/
+// const x = 'schnitzel' + 1;
+// console.log(x);
+
+/*
+Frage 9
+Was ist das Ergebnis?
+*/
+// const x = 'schnitzel' - 1;
+// console.log(x);
+
+// Everything else: https://www.destroyallsoftware.com/talks/wat

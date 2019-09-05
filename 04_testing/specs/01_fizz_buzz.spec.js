a73d728fbc0d18ea00ced0f50e1be73912c03083
Markus Kollers <m.kollers@vertical.de>
2019-09-05 16:46:54 +0200

    Fix wrong description

--- 04_testing/specs/01_fizz_buzz.spec.js
+++ 04_testing/specs/01_fizz_buzz.spec.js
@@ -8,19 +8,19 @@ describe('FizzBuzz', function () {
         });
     }
     for (const i of [5, 10, 20, 25, 35, 40]) {
-        it(`Should return Fizz for ${i}`, () => {
+        it(`Should return Buzz for ${i}`, () => {
             const output = fizzBuzz(i);
             expect(output).toEqual('Buzz');
         });
     }
     for (const i of [15, 30, 45, 60, 75, 90]) {
-        it(`Should return Fizz for ${i}`, () => {
+        it(`Should return FizzBuzz for ${i}`, () => {
             const output = fizzBuzz(i);
             expect(output).toEqual('FizzBuzz');
         });
     }
     for (const i of [1, 2, 4, 7, 8, 11]) {
-        it(`Should return Fizz for ${i}`, () => {
+        it(`Should return the number for ${i}`, () => {
             const output = fizzBuzz(i);
             expect(output).toEqual(i);
         });

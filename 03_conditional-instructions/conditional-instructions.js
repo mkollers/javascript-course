ce753a552ed369b03f5c71727fb5e6634475d2b1
Markus Kollers <mail@markuskollers.de>
2019-09-05 10:02:41 +0200

    Add some conditional instructiin questions

---
+++ 03_conditional-instructions/conditional-instructions.js
@@ -0,0 +1,48 @@
+/*
+Frage 1
+Was ist die Ausgabe?
+*/
+// if (42 % 14 === 0) {
+//     console.log('Foo');
+// } else {
+//     console.log('Bar');
+// }
+
+/*
+Frage 2
+Was ist die Ausgabe?
+*/
+// let result = '';
+// for (let i = 9; i > 0; i--) {
+//     result += i.toString();
+// }
+// console.log(result);
+
+/*
+Frage 3
+Was ist die Ausgabe?
+*/
+// for (let i in [3,2,1]) {
+//     console.log(i);
+// }
+
+/*
+Frage 4
+Was ist die Ausgabe?
+*/
+// const obj = {
+//     a: 1,
+//     b: 'Schnitzel'
+// };
+// for (let i in obj) {
+//     console.log(i);
+// }
+
+/*
+Frage 5
+Was ist die Ausgabe?
+*/
+var obj = {
+    a: 1,
+    b: 2
+} 

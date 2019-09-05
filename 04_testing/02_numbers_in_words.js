05a82aa8867bc2353a895a60812368f15be06f0f
Markus Kollers <m.kollers@vertical.de>
2019-09-05 16:52:13 +0200

    Add a coding dojo for learning

---
+++ 04_testing/02_numbers_in_words.js
@@ -0,0 +1,14 @@
+/*
+It occurs now and then in real life that people want to write about money, 
+especially about a certain amount of money. If it comes to cheques or contracts 
+for example some nations have laws that state that you should write out the
+amount in words additionally to the amount in numbers to avoid fraud and 
+mistakes. So if you want to transfer 745$ to someone via cheque you have to fill out 
+two fields:
+
+745.00$ (amount in numbers)
+seven hundred and fourty five dollars (amount in words)
+*/
+module.exports = function numberToWords(num) {
+    
+}
---
+++ 04_testing/specs/02_numbers_in_words.spec.js
@@ -0,0 +1,21 @@
+const numberToWords = require('../02_numbers_in_words');
+
+describe('FizzBuzz', function () {
+    // it('should return "seven hundred and fourty five dollars"', () => {
+    //     const output = numberToWords(745);
+    //     expect(output).toEqual('seven hundred and fourty five dollars');
+    // });
+    // it('should return "forty-two dollars"', () => {
+    //     const output = numberToWords(42);
+    //     expect(output).toEqual('forty-two dollars');
+    // });
+    // it('should return "one dollar"', () => {
+    //     const output = numberToWords(1);
+    //     expect(output).toEqual('one dollar');
+    // });
+    // it('should return "thirteen dollars"', () => {
+    //     const output = numberToWords(13);
+    //     expect(output).toEqual('thirteen dollars');
+    // });
+    // Define more test cases, like: decimals, numbers up to trillions
+});

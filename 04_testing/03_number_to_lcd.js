f56b719a1db8589b38b748915f2df430c16f906b
Markus Kollers <m.kollers@vertical.de>
2019-09-05 17:01:55 +0200

    Add blank additional dojo

---
+++ 04_testing/03_number_to_lcd.js
@@ -0,0 +1,7 @@
+/*
+Write a program that given a number (with arbitrary number of digits), converts it 
+into LCD style numbers using the following format:
+   _  _     _  _  _  _  _  
+ | _| _||_||_ |_   ||_||_|  
+ ||_  _|  | _||_|  ||_| _|  
+*/
